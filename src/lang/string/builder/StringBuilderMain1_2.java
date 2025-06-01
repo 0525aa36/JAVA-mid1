c334adecf01c6dba2a24422d6aa30d6831d73395
0525aa36 <0525aa36@gmail.com>
2025-06-02 00:48:06 +0900

    wrapper class start

---
+++ src/lang/string/builder/StringBuilderMain1_2.java
@@ -0,0 +1,15 @@
+package lang.string.builder;
+
+public class StringBuilderMain1_2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        String string = sb.append("A").append("B").append("C").append("D")
+                .insert(4, "Java")
+                .delete(4, 8)
+                .reverse()
+                .toString();
+        //메서드 체이닝
+        System.out.println("string = " + string);
+    }
+}
+//만드는 사람이 수고로우면 쓰는 사람이 편하고, 만드는 사람이 편하면 쓰는 사람이 수고롭다
